5f9f1c2af2a6cc2861d9453f7ccb44606fd30571
Trevyn Langsford <tlangsford@gmail.com>
2015-01-04 15:53:07 -0500

    Cleaned up.

--- src/pokeDoorDecs.groovy
+++ src/pokeDoorDecs.groovy
@@ -12,7 +12,6 @@ import java.awt.Font
 class Resident {
 
     String name
-    String pokeName
     BufferedImage sprite
     BufferedImage image
     Integer num
@@ -26,11 +25,9 @@ def http = new HTTPBuilder("http://pokeapi.co/")
 def names = new JsonSlurper().parse(new File("residents.json"))
 def residents = []
 
-
-
 names.each { name ->
     def resident = new Resident(name)
-    def num = Math.random() * 130
+    def num = Math.random() * 151
     num = (Integer) Math.ceil(num)
     resident.num = num
     residents.add(resident)
@@ -38,15 +35,12 @@ names.each { name ->
 
 println "Parsed ${residents.size()} residents."
 
-
 residents.each { resident ->
     try {
         http.get(path: "api/v1/pokemon/${resident.num}/",) { resp, info ->
-            resident.pokeName = info["name"]
 
             http.get(path: "api/v1/sprite/${resident.num}") { resp2, spriteInfo ->
                 def imgURL = spriteInfo["image"]
-                BufferedImage image
                 def url = new URL("http://pokeapi.co/${imgURL}")
                 resident.sprite = ImageIO.read(url)
 
@@ -62,21 +56,27 @@ residents.each { resident ->
 residents.each { resident ->
     def image = new BufferedImage((resident.sprite.getWidth() * 3) + 50 ,(resident.sprite.getHeight() * 3) + 70, BufferedImage.TYPE_INT_ARGB)
     def g = image.getGraphics()
-    g.setFont(new Font ("Garamond", Font.ITALIC , 50))
+    g.setFont(new Font ("Garamond", Font.ITALIC , 40))
 
     g.drawImage(resident.sprite, 25, 0, resident.sprite.getWidth() * 3 , resident.sprite.getHeight() * 3, null)
     g.setColor(Color.BLACK)
     g.drawString(resident.name, 50, (resident.sprite.getHeight() * 3) + 10)
 
-
     resident.image = image
-
 }
 
 println "Retrieved pokemon sprites."
 
+def dir = new File("docs")
+if (!dir.exists()) {
+    dir.mkdir()
+}
+dir.eachFile { file ->
+    file.delete()
+}
+
 residents.each { resident  ->
-    ImageIO.write(resident.image, "png", new File("${resident.name}.png"))
+    ImageIO.write(resident.image, "png", new File("decs/${resident.name}.png"))
 }
 
 
